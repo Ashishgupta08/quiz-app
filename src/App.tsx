942ec93a299e497e760eb925bcad8c73e61107a2
Ashish Gupta <59931129+Ashishgupta08@users.noreply.github.com>
2021-05-14 17:54:57 +0530

    init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="mx-auto my-1 p-2 bg-gray-50 rounded-2xl shadow-lg">
+      <h1 className="mx-auto px-5 text-black">QUIZO - Quiz App</h1>
+    </div>
+  );
+}
+
+export default App;
