18e7ee2ddc29282e90d1a13da7a2bb1ae184ed42
Ashish Gupta <59931129+Ashishgupta08@users.noreply.github.com>
2021-05-14 17:59:23 +0530

    dev init

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="mx-auto my-1 p-2 bg-gray-50 rounded-2xl shadow-lg">
-      <h1 className="mx-auto px-5 text-black">QUIZO - Quiz App</h1>
+      <h1 className="mx-auto px-5 text-black">QUIZO - A Quiz App</h1>
     </div>
   );
 }
